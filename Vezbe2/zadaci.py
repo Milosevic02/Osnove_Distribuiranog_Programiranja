7d875d183009124b86d90ce0c1e8b983e9a6b5b4
Dragan <milosevicdragan002@gmail.com>
2024-03-12 16:17:13 +0100

    zadatak3

--- Vezbe2/zadaci.py
+++ Vezbe2/zadaci.py
@@ -8,3 +8,13 @@ a = float(input("Unesi prvi broj:"))
 b = float(input("Unesi drugi broj:"))
 print("a je veci") if a > b else print("b je veci") if a < b else print("Jednaki su")
 
+#3
+povrce = ["grasak","krastavac"]
+if "krompir" not in povrce:
+    if "grasak" in povrce:
+        print("grasak se nalazi u listi")
+    else:
+        print("Vreme je za nabavku")
+else:
+    print("Krompir se nalazi u listi")
+    
