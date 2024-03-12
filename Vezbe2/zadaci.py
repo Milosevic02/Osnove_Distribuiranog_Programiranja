697be6fecaf7623caca9a41c3b3ca7cde9e3dd5d
Dragan <milosevicdragan002@gmail.com>
2024-03-12 16:17:03 +0100

    zadatak2

--- Vezbe2/zadaci.py
+++ Vezbe2/zadaci.py
@@ -3,3 +3,8 @@ broj = float(input("Unesi neki broj:"))
 if broj < 0 :
     print("Uneti broj je negativan")
     
+#2
+a = float(input("Unesi prvi broj:"))
+b = float(input("Unesi drugi broj:"))
+print("a je veci") if a > b else print("b je veci") if a < b else print("Jednaki su")
+
