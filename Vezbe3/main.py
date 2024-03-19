9958970d6ecef0b1badd710618c685c2a842dc5b
Dragan <milosevicdragan002@gmail.com>
2024-03-19 20:25:15 +0100

    Zadatak2

--- Vezbe3/main.py
+++ Vezbe3/main.py
@@ -22,3 +22,28 @@ print(student2)
 print(student3)
 print(student4)
 
+#2
+class Merenje:
+    drzava = "srbija"
+    def __init__(self,grad,merenja):
+        self.grad = grad
+        self.merenja = merenja
+        
+    def __str__(self):
+        return f"{self.drzava}, {self.grad}, {self.merenja}"
+
+    def prosek(self):
+        try:
+            return sum(self.merenja)/len(self.merenja)
+        except Exception as e:
+            print("Greska:",e)
+            return 0
+        
+novi_sad = Merenje("Novi Sad", [1.75, 1.80, 1.65, 2.02, 1.90, 1.68, 1.73])
+sombor = Merenje("Sombor", [1.85, 1.80, 1.75, 2.02, 1.95, 1.78, 1.83])
+zrenjanin = Merenje("Zrenjanin", [1.75, 1.70, 1.67, 2.00, 1.80, 1.78, 1.73])
+
+print("Prosecna visina po gradu:")
+print(f"{novi_sad}, prosečna visina: {novi_sad.prosek()}")
+print(f"{sombor}, prosečna visina: {sombor.prosek()}")
+print(f"{zrenjanin}, prosečna visina: {zrenjanin.prosek()}")
