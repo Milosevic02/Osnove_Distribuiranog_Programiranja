3a1d81749f3941c8856d0566e2f12683d23cd8a8
Dragan <milosevicdragan002@gmail.com>
2024-04-19 17:57:00 +0200

    Add func

--- PripremaK1/server.py
+++ PripremaK1/server.py
@@ -1,4 +1,21 @@
-import socket
+import socket,pickle
+
+profesori = {}
+
+def log_info(message):
+    log = open("log.txt","a")
+    log.write(message + "\n")
+    log.close()
+    
+def dodaj_lek(poruka):
+    profesor = pickle.loads(poruka)
+    if profesor.jmbg in profesori:
+        odgovor = f"Profesor sa jmbg {profesor.jmbg} vec postoji u bazi!"
+    else:
+        profesori[profesor.jmbg] = profesor
+        odgovor = f"Profesor sa jmbg {profesor.jmbg}  je sacuvan u bazu!"
+    log_info(odgovor)
+    return odgovor.encode()
 
 server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 server.bind(('localhost',6000))
@@ -9,6 +26,14 @@ kanal,adresa = server.accept()
 print(f"Prihvacena je konekcija sa adrese: {adresa}")
 
 while True:
-    poruka = kanal.recv(1024).decode()
-    if not poruka :break
-    print("Stigla poruka:",poruka)
+    opcija = kanal.recv(1024).decode()
+    if not opcija :break
+    match opcija:
+        case "ADD":
+            odgovor = dodaj_lek(kanal.recv(1024))
+            
+    kanal.send(odgovor)
+    
+    
+print("Server se gasi")    
+server.close()
