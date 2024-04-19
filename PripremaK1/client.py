ce9feb28a4d39e4a36f7e5ac78f8661221bfbb1e
Dragan <milosevicdragan002@gmail.com>
2024-04-19 17:35:52 +0200

    Write and send json file

--- PripremaK1/client.py
+++ PripremaK1/client.py
@@ -1,14 +1,30 @@
-import socket,pickle
+import socket,pickle,json
 from datetime import date
 from profesor import Profesor
 
+#1,2
 def pokupi_objekat():
     jmbg = input("jmbg leka -> ")
     ime = input("Ime ->")
     prezime = input("Prezime->")
     godina, mesec, dan = input("Godina, mesec, dan proizvodnje u obliku YYYY-MM-DD -> ").split('-')
     profesor = Profesor(jmbg,ime,prezime,date(int(godina),int(mesec),int(dan)))
-    return pickle.dumps(profesor)
+    f = open("profesor.json",'w')
+    json.dump(profesor.__dict__,f)
+    f.close()
+    
+    f = open("profesor.json")
+    profesor_json = json.load(f)
+    f.close()
+    
+    return pickle.dumps(profesor_json)
+
+#3,4,6
+def pokupi_jmbg():
+    return input("JMBG ->").encode()
+
+#5
+
 
 klijent = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 klijent.connect(('localhost',6000))
