be50c6887f560f8eaaf916814d6db12d660d59d2
Dragan <milosevicdragan002@gmail.com>
2024-04-19 10:22:21 +0200

    info for object func

--- PripremaK1/client.py
+++ PripremaK1/client.py
@@ -1,4 +1,14 @@
-import socket
+import socket,pickle
+from datetime import date
+from profesor import Profesor
+
+def pokupi_objekat():
+    jmbg = input("jmbg leka -> ")
+    ime = input("Ime ->")
+    prezime = input("Prezime->")
+    godina, mesec, dan = input("Godina, mesec, dan proizvodnje u obliku YYYY-MM-DD -> ").split('-')
+    profesor = Profesor(jmbg,ime,prezime,date(int(godina),int(mesec),int(dan)))
+    return pickle.dumps(profesor)
 
 klijent = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
 klijent.connect(('localhost',6000))
