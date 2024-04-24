6ae9d11188f87e191f68d933f5d6a0443990a60f
Dragan <milosevicdragan002@gmail.com>
2024-04-24 08:40:21 +0200

    Add lek func

--- Vezbe5/server.py
+++ Vezbe5/server.py
@@ -6,3 +6,13 @@ def log_info(message):
     log = open("log.txt", "a")
     log.write(message + "\n")
     log.close()
+    
+def dodaj_lek(poruka):
+    lek = pickle.loads(poruka)
+    if lek.id in lekovi:
+        odgovor = f"Lek sa id-em: {lek.id} vec postoji u bazi!"
+    else:
+        lekovi[lek.id] = lek
+        odgovor = f"Lek sa id-em: {lek.id} uspesno upisan u bazu."
+    log_info(odgovor)
+    return odgovor.encode()
