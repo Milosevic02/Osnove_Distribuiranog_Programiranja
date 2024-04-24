c1470d5ab058fcc80c6c639d130ffc5cdd9b0afc
Dragan <milosevicdragan002@gmail.com>
2024-04-24 08:45:54 +0200

    Menu for server

--- Vezbe5/server.py
+++ Vezbe5/server.py
@@ -55,3 +55,35 @@ def dodaj_sastojke(id, podaci):
         print(odgovor)
     log_info(odgovor)
     return odgovor.encode()
+
+def main():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind(('localhost', 6000))
+    server.listen()
+    print("Server je pokrenut.")
+
+    kanal, adresa = server.accept()
+    print(f"Prihvacena je konekcija sa adrese: {adresa}")
+
+    while True: 
+        opcija = kanal.recv(1024).decode()
+        if not opcija : break
+        if opcija == "ADD": # Dodaj lek
+            odgovor = dodaj_lek(kanal.recv(1024))
+        elif opcija == "UPDATE": # Izmeni lek
+            odgovor = izmeni_lek(kanal.recv(1024))
+        elif opcija == "DELETE": # Obrisi lek
+            odgovor = izbrisi_lek(kanal.recv(1024).decode())
+        elif opcija == "READ": # Procitaj lek
+            odgovor = procitaj_lek(kanal.recv(1024).decode())            
+        elif opcija == "ADD_INGR": # Dodaj sastojke
+            id = kanal.recv(1024).decode()
+            sastojci = kanal.recv(1024)
+            odgovor = dodaj_sastojke(id, sastojci)        
+        kanal.send(odgovor)
+    
+    print("Server se gasi.")
+    server.close()
+
+
+main()
