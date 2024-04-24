bdb47df0f48a48cc781fc6c84491b1eb8f4faec4
Dragan <milosevicdragan002@gmail.com>
2024-04-24 08:41:20 +0200

    add sastojke func

--- Vezbe5/server.py
+++ Vezbe5/server.py
@@ -44,3 +44,14 @@ def procitaj_lek(id):
     else:
         odgovor = pickle.dumps(lekovi[id])
         return odgovor
+    
+def dodaj_sastojke(id, podaci):
+    if id not in lekovi:
+        odgovor = f"Lek sa id-em: {id} ne postoji u bazi!"
+    else:
+        sastojci = pickle.loads(podaci)
+        lekovi[id].sastojci.extend(sastojci)
+        odgovor = f"Uspesno dodati sastojci za lek sa id-em: {id}."
+        print(odgovor)
+    log_info(odgovor)
+    return odgovor.encode()
