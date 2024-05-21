d750db25e64cc3353befa39e379c1f1a30ffe281
Dragan <milosevicdragan002@gmail.com>
2024-05-21 16:28:33 +0200

    Create monitor.py

---
+++ PripremaK2/Z2/monitor.py
@@ -0,0 +1,26 @@
+import socket
+
+def azuriraj_stanje(socket,stanje):
+    socket.send(stanje.encode())
+    print(socket.recv(1024).decode())
+    
+    
+def main():
+    monitorP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    monitorS = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    
+    try:
+        monitorP.connect(('localhost',6000))
+        print("Veza sa primarnim serverom je uspostavljena.")
+
+        azuriraj_stanje(monitorP,'primarni')
+    except Exception as ex:
+        print(ex)
+        
+    try:
+        monitorS.connect(('localhost',7000))
+        print("Veza sa sekundarnim serverom je uspostavljena.")
+
+        azuriraj_stanje(monitorS, "sekundarni")
+    except Exception as ex:
+        print(ex)
