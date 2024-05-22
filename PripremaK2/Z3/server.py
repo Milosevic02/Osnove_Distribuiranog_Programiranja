38bf90375bd639f4e60de6e9e31a80a7d8a5634e
Dragan <milosevicdragan002@gmail.com>
2024-05-22 17:37:34 +0200

    Edit func

--- PripremaK2/Z3/server.py
+++ PripremaK2/Z3/server.py
@@ -10,4 +10,16 @@ def dodaj_lice(poruka):
         fizickaLica[lice.jmbg] = lice
         odgovor = f"Lice sa jmbg-om: {lice.jmbg} uspesno upisan u bazi!"
     return odgovor.encode()
+
+
+def izmeni_lice(poruka):
+    lice = pickle.loads(poruka)
+    if lice.jmbg not in fizickaLica:
+        odgovor = f"Lice sa jmbg-om: {lice.jmbg} ne postoji u bazi!"
+    else:
+        fizickaLica[lice.jmbg] = lice
+        odgovor = f"Lice sa jmbg-om: {lice.jmbg} uspesno izmenjen u bazi!"
+        
+    return odgovor.encode()
+
     
