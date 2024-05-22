ec0aac2c68f89941daea9ae7efee45b2f2ddcbaa
Dragan <milosevicdragan002@gmail.com>
2024-05-22 19:11:47 +0200

    Add client menu

--- PripremaK2/Z3/client.py
+++ PripremaK2/Z3/client.py
@@ -33,3 +33,65 @@ def iscitaj_sva_lica(odgovor):
         print(lista)
     except:
         print(odgovor.decode())
+    
+        
+        
+def main():
+    klijentP = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    klijentP.connect(('localhost',6000))
+    print("Veza sa primarnim serverom je uspostavljena")
+    
+    while not login(klijentP):
+        login(klijentP)
+        
+    while True: 
+        operacija = input("Odaberite operaciju: \n1.Dodaj lice \n2.Izmeni lice \n3.Obrisi lice\n4.Procitaj lice\n5.Procitaj Sortiranu Listu Lica\n6.Repliciraj podatke\n") 
+        if not operacija : break        
+        if operacija == "1": # Dodaj lek   
+            klijentP.send(("ADD").encode())
+            odgovor = klijentP.recv(1024).decode()
+            if odgovor == "False":
+                print("Nemate prava za ovu opciju izaberite neku drugu")
+            else:
+                klijentP.send(pokupi_informacije_lica_za_slanje())
+                print(klijentP.recv(1024).decode())
+                
+        elif operacija == "2": # Izmeni lek 
+            klijentP.send(("UPDATE").encode())
+            odgovor = klijentP.recv(1024).decode()
+            if odgovor == "False":
+                print("Nemate prava za ovu opciju izaberite neku drugu")
+            else:
+                klijentP.send(pokupi_informacije_lica_za_slanje())
+                print(klijentP.recv(1024).decode())
+        elif operacija == "3": # Obrisi lek 
+            klijentP.send(("DELETE").encode())
+            odgovor = klijentP.recv(1024).decode()
+            if odgovor == "False":
+                print("Nemate prava za ovu opciju izaberite neku drugu")
+            else:
+                klijentP.send(pokupi_informaciju_jmbg_lica_za_slanje())
+                print(klijentP.recv(1024).decode())     
+        elif operacija == "4": # Procitaj lek 
+            klijentP.send(("READ").encode())
+            odgovor = klijentP.recv(1024).decode()
+            if odgovor == "False":
+                print("Nemate prava za ovu opciju izaberite neku drugu")
+            else: 
+                klijentP.send(pokupi_informaciju_jmbg_lica_za_slanje())
+                iscitaj_lice(klijentP.recv(1024))
+        elif operacija == "5":
+            klijentP.send(("READALL").encode())
+            odgovor = klijentP.recv(1024).decode()
+            if odgovor == "False":
+                print("Nemate prava za ovu opciju izaberite neku drugu")
+            else: 
+                iscitaj_sva_lica(klijentP.recv(1024))
+        else:
+            print("Molimo unesite validnu operaciju.")
+            continue
+
+    klijentP.close() 
+    print("Zatvaranje konekcije.")
+    
+main()
