a0947b8983c522037cf812eceeee684f40954e96
Dragan <milosevicdragan002@gmail.com>
2024-05-22 18:21:29 +0200

    Fix bug

--- PripremaK2/Z1/direktorijum_korisnika.py
+++ PripremaK2/Z1/direktorijum_korisnika.py
@@ -7,7 +7,7 @@ def hesiranje(tekst):
     return hashlib.sha256(tekst.encode()).hexdigest()
 
 def dodaj_korisnika(korisnicko_ime,lozinka):
-    korisnici[korisnicko_ime] - Korisnik(korisnicko_ime,hesiranje(lozinka))
+    korisnici[korisnicko_ime] = Korisnik(korisnicko_ime,hesiranje(lozinka))
     
 def autentifikacija(korisnicko_ime,lozinka):
     if(korisnicko_ime in korisnici) and (hesiranje(lozinka) == korisnici[korisnicko_ime].lozinka):
